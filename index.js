2832b555cf8e5678e11dd247a6b59f3532da3e9d
karinadesposito <110677129+karinadesposito@users.noreply.github.com>
2023-06-11 00:47:03 -0300

    versión sólo cambios css

--- index.js
+++ index.js
@@ -1,34 +1,50 @@
 const form = document.getElementById("form");
+
+
+
+
+
+
+
+let idSelect = null;
+let accion = "";
 function handleSubmit(event) {
-    event.preventDefault(); // Evita el envío del formulario 
-    const user = new FormData(form);
-    const newUser = {
-      name: user.get("name"),
-      email: user.get("email"),
-      phone: user.get("phone")
-    };
-    addOne(newUser);
-    form.reset();
-  }
-  form.addEventListener("submit", handleSubmit);
-const openModal = document.getElementById("open-modal");
+  event.preventDefault(); // Evita el envío del formulario
+  const user = new FormData(form);
+  const newUser = {
+    name: user.get("name"),
+    email: user.get("email"),
+    phone: user.get("phone"),
+  };
+  if (accion!= "" && accion =="add" )
+  addOne(newUser);
+  if (accion!= "" && accion =="modified")
+  editarEmpresa(idSelect, newUser);
+ modal.close();
+ accion = "";
+  form.reset();
+}
+form.addEventListener("submit", handleSubmit);
+const agregarEmpresa = document.getElementById("agregarEmpresa");
 const modal = document.getElementById("modal");
 const closeModal = document.getElementById("close-modal");
-closeModal.addEventListener("click", () => {
+closeModal && closeModal.addEventListener("click", () => {
   modal.close();
 });
-openModal.addEventListener("click", () => {
+agregarEmpresa.addEventListener("click", () => {
+  accion = "add";
   modal.showModal();
 });
 const tabla = document.getElementById("miTabla");
 const baseUrl = "https://647a6fb6d2e5b6101db05b10.mockapi.io/listado";
 function getMockapi() {
   fetch(baseUrl)
-    .then(response => response.json())
-    .then(data => {
-      data.forEach(item => {
+    .then((response) => response.json())
+    .then((data) => {
+      data.forEach((item) => {
         const fila = document.createElement("tr");
         const nombre = document.createElement("td");
+        fila.dataset.empresaId = item.id;
         nombre.textContent = item.name;
         fila.appendChild(nombre);
         const email = document.createElement("td");
@@ -46,7 +62,9 @@ function getMockapi() {
         btnModificar.classList.add("btnModificar");
         btnModificar.dataset.empresaId = item.id;
         acciones.appendChild(btnModificar);
-        btnModificar.addEventListener("click", () => {
+        btnModificar.addEventListener("click", () => {          
+          idSelect = item.id;
+          accion = "modified";
           abrirFormConValores(item);
         });
         const btnEliminar = document.createElement("button");
@@ -55,7 +73,7 @@ function getMockapi() {
         btnEliminar.dataset.empresaId = item.id;
         acciones.appendChild(btnEliminar);
         btnEliminar.addEventListener("click", () => {
-          const id = btnEliminar.dataset.empresaId;
+          const id = btnEliminar.dataset.empresaId;          
           deleteOne(id);
         });
         fila.appendChild(acciones);
@@ -64,13 +82,15 @@ function getMockapi() {
     });
 }
 function abrirFormConValores(data) {
-  modal.showModal();
+
   const nameField = document.getElementById("name");
   const emailField = document.getElementById("email");
   const phoneField = document.getElementById("phone");
+
   nameField.value = data.name;
   emailField.value = data.email;
   phoneField.value = data.phone;
+  modal.showModal();
 }
 // function getAll() {
 //   fetch(baseUrl)
@@ -86,38 +106,34 @@ function abrirFormConValores(data) {
 // }
 function deleteOne(id) {
   fetch(baseUrl + `/${id}`, {
-    method: "DELETE"
+    method: "DELETE",
   })
-    .then(response => response.json())
-    .then(data => console.log(data))
-    .catch(err => console.error(err));
+    .then((response) => response.json())
+    .then((data) => console.log(data))
+    .catch((err) => console.error(err));
 }
 function addOne(user) {
   fetch(baseUrl, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(user)
+    body: JSON.stringify(user),
   })
-    .then(response => response.json())
-    .then(data => console.log(data))
-    .catch(err => console.error(err));
+    .then((response) => response.json())
+    .then((data) => console.log(data))
+    .catch((err) => console.error(err));
 }
 function editarEmpresa(id, item) {
   fetch(baseUrl + `/${id}`, {
     method: "PUT",
     headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(item)
+    body: JSON.stringify(item),
   })
-    .then(response => response.json())
-    .then(data => {
-      console.log(data);
-      const nameField = document.getElementById("name");
-      const emailField = document.getElementById("email");
-      const phoneField = document.getElementById("phone");
-      nameField.value = data.name;
-      emailField.value = data.email;
-      phoneField.value = data.phone;
+    .then((response) => response.json())
+    .then((data) => {
+      const tr = document.querySelector(`tr[data-empresa-id = '${id}']`)
+      console.log("responseEditar", data);
+     
     })
-    .catch(err => console.error(err));
+    .catch((err) => console.error(err));
 }
 getMockapi();
